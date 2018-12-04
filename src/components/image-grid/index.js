df21208c9d76f879f2306a6d17c4958f58c2e433
JOsktgui <jose.osk01@gmail.com>
2018-12-03 23:15:34 -0500

    adding header and images

---
+++ src/components/image-grid/index.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+const key = '################################';
+
+class ImageGrid extends Component {
+  state = {
+    images: [],
+  };
+
+  componentDidMount() {
+    fetch(`https://api.unsplash.com/photos/?client_id=${key}&per_page=28`)
+      .then(res => res.json())
+      .then(images => {
+        this.setState({
+          images,
+        });
+      });
+  }
+
+  render() {
+    const { images } = this.state;
+    return (
+      <div className="content">
+        <section className="grid">
+          {images.map(image => (
+            <div
+              key={image.id}
+              className={`item item-${Math.ceil(
+                  image.height / image.width,
+              )}`}
+            >
+              <img
+                  src={image.urls.small}
+                  alt={image.user.username}
+              />
+            </div>
+          ))}
+        </section>
+      </div>
+    );
+  }
+}
+
+export default ImageGrid;
